744484caec3c8dfe723093fdc1258f1b44668f8f
adamw <adam@warski.org>
2016-07-26 17:02:14 +0200

    Fixes

--- src/main/scala/com/softwaremill/its/Testing.scala
+++ src/main/scala/com/softwaremill/its/Testing.scala
@@ -26,18 +26,20 @@ object Testing extends App {
     .filter(_.isValid)
     .zip(Source.unfold(0)(st => Some((st+1, st+1))))
     .map { case (t, i) =>
-    if (i%100 == 0) println(s"Processing trip $i")
+    if (i%1000 == 0) println(s"Processing trip $i")
     t
   }
     .fold(HotspotState(Nil, Nil)) { case (st, t) => st.addTrip(t) }
     .map(_.result())
     .runForeach { hotspots =>
+      val sortedHotspots = hotspots.sortBy(_.count)
+      sortedHotspots.foreach(println)
       println(s"Found ${hotspots.size} hotspots")
     }
 
   import scala.concurrent.duration._
 
-  try Await.result(f, 1.minute)
+  try Await.result(f, 10.minutes)
   finally as.terminate()
 }
 
@@ -57,7 +59,16 @@ object Trip {
 }
 
 // A box spans from <x, y> to (x+step, y+step). Left and top borders are inclusive, right- and bottom are exclusive.
-case class GridBox(x: Int, y: Int)
+case class GridBox(x: Int, y: Int) {
+  import GridBox._
+
+  def center: (Double, Double) = ((x+BoxUnits/2.0d) * LatUnit, (y+BoxUnits/2.0d) * LngUnit)
+
+  override def toString = {
+    val (lat, lng) = center
+    f"($lat%2.6f, $lng%2.6f)"
+  }
+}
 
 object GridBox {
   // each degree of latitude is approximately 69 miles (111 kilometers) apart; 100m = 0.0009
@@ -100,7 +111,7 @@ object WindowBounds {
 
   def boundsFor(t: OffsetDateTime): List[WindowBounds] = {
     val firstBoundMinute = t.getMinute-t.getMinute%StepLengthMinutes
-    val firstBoundStart = t.withMinute(firstBoundMinute)
+    val firstBoundStart = t.withMinute(firstBoundMinute).withSecond(0).withNano(0)
     (for (i <- 0 until WindowsPerTimepoint) yield
       WindowBounds(
         firstBoundStart.plusMinutes(i*StepLengthMinutes),
@@ -109,34 +120,36 @@ object WindowBounds {
   }
 }
 
-case class Hotspot()
+case class Hotspot(bounds: WindowBounds, box: GridBox, count: Int, neighborCounts: List[Int])
 
 case class Window(bounds: WindowBounds, boxCounts: Map[GridBox, Int]) {
   import Window._
 
   def addBox(gb: GridBox): Window = copy(boxCounts = boxCounts.updated(gb, boxCounts.getOrElse(gb, 0)+1))
   def close(): List[Hotspot] = {
-    //println(s"Closing window $bounds")
     boxCounts.flatMap { case (box, count) =>
       if (count < CountThreshold) None else {
-        println("CANDIDATE " + count)
         val neighborCounts = NeighborhoodOffsets.map { offset =>
           val neighborBox = box.copy(box.x+offset._1, box.y+offset._2)
           boxCounts.getOrElse(neighborBox, 0)
         }
 
         if (neighborCounts.forall(nghCount => nghCount * NeighborsMultiplierThreshold <= count)) {
-          println("HOTSPOT! " + neighborCounts)
-          Some(Hotspot())
-        } else None
+          println("HOTSPOT " + count)
+          val hotspot = Hotspot(bounds, box, count, neighborCounts)
+          Some(hotspot)
+        } else {
+          println("NON-HOTSPOT CANDIDATE " + count)
+          None
+        }
       }
     }
   }.toList
 }
 
 object Window {
-  val CountThreshold = 25
-  val NeighborsMultiplierThreshold = 2.0d
+  val CountThreshold = 40
+  val NeighborsMultiplierThreshold = 2.5d
   val NeighborhoodOffsets: List[(Int, Int)] = {
     import GridBox._
     val offsets = List(-BoxUnits, 0, BoxUnits)
